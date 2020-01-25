85e1bae5b377b790c7a85e0b8182b2b31b47c519
lev <nicki070898@gmail.com>
2020-01-26 00:41:04 +0300

    download voice message from telegram

--- core/telegram_api.py
+++ core/telegram_api.py
@@ -4,10 +4,17 @@ import logging
 from DSP_bot import settings
 
 
+URL = 'https://api.telegram.org'
 logger = logging.getLogger(__name__)
 
+
 def invoke_telegram(method, **kwargs):
-    url = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/{method}"
+    url = f"{URL}/bot{settings.TELEGRAM_BOT_TOKEN}/{method}"
     resp = requests.post(url, data=kwargs, timeout=(3.05, 27), proxies=settings.PROXY)
     logger.info("Response %s %s" % (resp, resp.content))
     return resp
+
+
+def download_file(file_path):
+    resp = requests.get(f'{URL}/file/bot{settings.TELEGRAM_BOT_TOKEN}/{file_path}', proxies=settings.PROXY)
+    return resp.content
--- core/views.py
+++ core/views.py
@@ -4,12 +4,23 @@ from django.views.decorators.csrf import csrf_exempt
 
 from django.http import HttpResponse
 
-from core.telegram_api import invoke_telegram
+from core.telegram_api import invoke_telegram, download_file
 
 
 @csrf_exempt
 def telegram_hook(request):
     update = json.loads(request.body)
+    message = update.get('message')
+
+    if message is not None and 'voice' in message.keys():
+        file_id = message['voice']['file_id']
+        file_path = get_telegram_file_path(file_id)
+        voice_message = download_file(file_path)
+
     invoke_telegram('sendMessage', chat_id=update['message']['chat']['id'], text='OK')
 
     return HttpResponse('OK')
+
+
+def get_telegram_file_path(file_id):
+    return json.loads(invoke_telegram('getFile', file_id=file_id).content)['result']['file_path']
