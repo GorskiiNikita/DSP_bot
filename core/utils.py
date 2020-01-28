38a6a7abcfae94aa57dd7e19b000d6ed09745a7d
lev <nicki070898@gmail.com>
2020-01-29 02:31:36 +0300

    convert audio to WAV

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 
 def get_number_file(path):
@@ -13,11 +14,33 @@ def make_dirs(path):
 
 
 def write_audio_file(file, user_id):
-    path = f'media/audio/{user_id}'
+    path = f'media/audio/{user_id}/oga'
 
-    make_dirs(f'{path}/oga')
+    make_dirs(path)
 
-    number = get_number_file(f'{path}/oga')
+    number = get_number_file(path)
+    filename = f'audio_message_{number}'
 
-    with open(f'{path}/oga/audio_message_{number}', 'wb') as f:
+    with open(f'{path}/{filename}', 'wb') as f:
         f.write(file)
+
+    return f'{path}/{filename}'
+
+
+def convert_from_opus(file_src, dest, filename):
+    subprocess.run(['ffmpeg', '-i', file_src, f'{dest}/{filename}.wav'])
+
+
+def convert_audio_file(file_src, user_id):
+    dest = f'media/audio/{user_id}/wav'
+    filename = file_src.split('/')[-1]
+
+    make_dirs(dest)
+
+    convert_from_opus(file_src, dest, filename)
+
+
+
+
+
+
--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
 from core.telegram_api import invoke_telegram, download_file
-from core.utils import write_audio_file
+from core.utils import write_audio_file, convert_audio_file
 
 
 @csrf_exempt
@@ -18,7 +18,8 @@ def telegram_hook(request):
         file_id = message['voice']['file_id']
         file_path = get_telegram_file_path(file_id)
         voice_message = download_file(file_path)
-        write_audio_file(voice_message, user_id)
+        file_src = write_audio_file(voice_message, user_id)
+        convert_audio_file(file_src, user_id)
 
     invoke_telegram('sendMessage', chat_id=update['message']['chat']['id'], text='OK')
 
