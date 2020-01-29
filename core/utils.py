10d823b440c6d6c866525d3bcb6951b00467f299
lev <nicki070898@gmail.com>
2020-01-29 03:03:50 +0300

    downsampling audio to 16KHz

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+import soundfile as sf
+import scipy.signal
 
 
 def get_number_file(path):
@@ -13,31 +15,35 @@ def make_dirs(path):
         pass
 
 
-def write_audio_file(file, user_id):
-    path = f'media/audio/{user_id}/oga'
-
-    make_dirs(path)
-
-    number = get_number_file(path)
-    filename = f'audio_message_{number}'
-
-    with open(f'{path}/{filename}', 'wb') as f:
-        f.write(file)
-
-    return f'{path}/{filename}'
-
-
 def convert_from_opus(file_src, dest, filename):
     subprocess.run(['ffmpeg', '-i', file_src, f'{dest}/{filename}.wav'])
+
+
+def write_audio_file(file, user_id):
+    path = f'media/audio/{user_id}/oga'
+    make_dirs(path)
+    number = get_number_file(path)
+    filename = f'audio_message_{number}'
+    with open(f'{path}/{filename}', 'wb') as f:
+        f.write(file)
+    return f'{path}/{filename}'
 
 
 def convert_audio_file(file_src, user_id):
     dest = f'media/audio/{user_id}/wav'
     filename = file_src.split('/')[-1]
-
     make_dirs(dest)
-
     convert_from_opus(file_src, dest, filename)
+    audio_data, samplerate = sf.read(f'{dest}/{filename}.wav')
+    new_audio_data = scipy.signal.decimate(audio_data, samplerate//16000)
+    sf.write(f'{dest}/{filename}.wav', new_audio_data, 16000)
+
+
+
+
+
+
+
 
 
 
