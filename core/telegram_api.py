bc3cf01a8e4c4a0e282876c043003429aaf9767f
lev <nicki070898@gmail.com>
2020-01-30 18:41:41 +0300

    delete proxies

--- core/telegram_api.py
+++ core/telegram_api.py
@@ -8,12 +8,14 @@ URL = 'https://api.telegram.org'
 logger = logging.getLogger(__name__)
 
 
+# Для Российских серверов нужно добавить параметр proxies в requests.post()
 def invoke_telegram(method, **kwargs):
-    resp = requests.post(f'{URL}/bot{settings.TELEGRAM_BOT_TOKEN}/{method}', data=kwargs, proxies=settings.PROXY)
+    #
+    resp = requests.post(f'{URL}/bot{settings.TELEGRAM_BOT_TOKEN}/{method}', data=kwargs)
     logger.info('Response %s %s' % (resp, resp.content))
     return resp
 
 
 def download_file(file_path):
-    resp = requests.get(f'{URL}/file/bot{settings.TELEGRAM_BOT_TOKEN}/{file_path}', proxies=settings.PROXY)
+    resp = requests.get(f'{URL}/file/bot{settings.TELEGRAM_BOT_TOKEN}/{file_path}')
     return resp.content
